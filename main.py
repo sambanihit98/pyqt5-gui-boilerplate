d48f6d5aee8342042951f21c646bdade6cc197f1
sambanihit98 <sambanihit@gmail.com>
2026-02-04 19:34:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Python Program")
 
 def main():
     app = QApplication(sys.argv)
