a9efde1f5da4c6a48df0e6f30379a88cdab94fe2
sambanihit98 <sambanihit@gmail.com>
2026-02-04 19:46:32 +0800

    Added icon

--- main.py
+++ main.py
@@ -4,12 +4,14 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Python Program")
+        self.setWindowTitle("SammiDodo")
         self.setGeometry(700, 300, 500, 500)
+        self.setWindowIcon(QIcon("chick.png"))
 
 def main():
     app = QApplication(sys.argv)
